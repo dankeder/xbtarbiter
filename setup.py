c9795213f212f6a0481fdb7293764c8fd2eea543
Dan Keder <dan.keder@gmail.com>
2014-06-05 11:08:06 +0200

    setup.py: Improve get_version()
    Print an error message if version cannot be determined.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 from setuptools import setup, find_packages
 from subprocess import Popen, PIPE
 
@@ -16,8 +17,12 @@ def get_version():
         # Try to get the version from the latest git tag
         p = Popen(['git', 'describe', '--tags'], stdout=PIPE, stderr=PIPE)
         p.stderr.close()
-        line = p.stdout.readlines()[0]
-        return line.strip()
+        lines = p.stdout.readlines()
+        try:
+            return lines[0].strip()
+        except IndexError:
+            sys.stderr.write('Cannot determine version. Try running "git fetch --tags" first.\n')
+            sys.exit(1)
 
 setup(name='xbtarbiter',
         version=get_version(),
